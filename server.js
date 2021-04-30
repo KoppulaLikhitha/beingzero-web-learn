c409a427f0c352c6e620fdeb942ac6d647e956b4
KoppulaLikhitha <likhithakoppula912@gmail.com>
2021-04-30 10:46:39 +0530

    css,java files are added

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
  
 const app = express();
  
+app.use(express.static(__dirname+"/frontend"));
+
 app.get("/", function(req, res){
     res.send("Welcome to Likhitha's Basic Site");
 })
 
 app.get("/resume", function(req, res){
-    res.send("Welcome to Likhitha's Resume");
+    //res.send("Welcome to Likhitha's Resume");
+    let vari =__dirname + "/frontend/resume.html";
+    res.sendFile(vari);
+
 })
  
 // Heroku will automatically set an environment variable called PORT
